1a9e56ea14936379dda2c33e4055586224518b94
Pedro <pedrolrc13@gmail.com>
2022-10-26 23:59:57 -0300

    ajuste

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, Response, request
 import cv2
-import datetime, time
-import os, sys
-import numpy as np
 from threading import Thread
 from vision import detect_text
  
